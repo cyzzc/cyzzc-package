87d735a6c2843195c01b25ba5092d0cd272e0f08
cyzzc <80954721+cyzzc@users.noreply.github.com>
2023-01-16 00:00:11 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -18,7 +18,7 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 git clone --depth 1 https://github.com/AlexZhuo/luci-app-bandwidthd.git
-git clone --depth 1 https://github.com/sirpdboy/netspeedtest && mvdir netspeedtest
+# git clone --depth 1 https://github.com/sirpdboy/netspeedtest && mvdir netspeedtest
 
 git clone --depth 1 https://github.com/kiddin9/my-packages && mvdir my-packages
 git clone --depth 1 https://github.com/kiddin9/openwrt-bypass && mvdir openwrt-bypass
